397917f092dec5ca4ce41dea3030162c620871af
Mariana3B <00001113151997SP@al.educacao.sp.gov.br>
2024-11-13 08:54:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "O Princípe"?',
+    'Maquíavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    '2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
